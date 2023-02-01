8a1663ec370c884ff24271a4ba2b593051ea6eac
vasilyl1 <likhovaido@gmail.com>
2023-02-01 09:36:52 -0500

    feature: dummy GPX points getAthleteGPX function code

---
+++ assets/src/js/config.js
@@ -0,0 +1,8 @@
+let stravaSecret = {
+    clientSecret : "e811ddb2de5d6a21d6c0ccc18cd48f27dc370274",
+    client_id : 101485,
+    redirect_uri : "https://tegrty.github.io",
+    code : "0c85c85dce1007eb348df786a8f3e888778b3a15", // short live time, obtained from redirect
+
+    // API key changed 
+}
